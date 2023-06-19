8d8ba252b42e79c1a1d3958ed065277913d9a460
PriyangshuJS <pjscse@gmail.com>
2023-06-19 20:31:10 +0530

    Bottom App Bar

--- lib/responsive/mobile_layout.dart
+++ lib/responsive/mobile_layout.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram/models/user.dart' as model;
 import 'package:instagram/provider/user_provider.dart';
+import 'package:instagram/utility/colors.dart';
 import 'package:provider/provider.dart';
 
 // ignore: camel_case_types
@@ -14,11 +16,30 @@ class MobileLayout extends StatefulWidget {
 }
 
 class _MobileLayoutState extends State<MobileLayout> {
+  late PageController pageController;
+  int _page = 0;
   String username = "";
   @override
   void initState() {
     super.initState();
     getusername();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
+  void navigationTapped(int page) {
+    pageController.jumpToPage(page);
+  }
+
+  void onPageChange(int page) {
+    setState(() {
+      _page = page;
+    });
   }
 
   void getusername() async {
@@ -35,8 +56,73 @@ class _MobileLayoutState extends State<MobileLayout> {
   Widget build(BuildContext context) {
     model.User user = Provider.of<UserProvider>(context).getUser;
     return Scaffold(
-      body: Center(
-        child: Text(user.username),
+      body: PageView(
+        children: [
+          Center(
+            child: Text("${user.username} - Home"),
+          ),
+          Center(
+            child: Text("Seach"),
+          ),
+          Center(
+            child: Text("Add"),
+          ),
+          Center(
+            child: Text("Favorites"),
+          ),
+          Center(
+            child: Text("Profile"),
+          )
+        ],
+        controller: pageController,
+        onPageChanged: onPageChange,
+        physics: const NeverScrollableScrollPhysics(),
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: mobileBackgroundColor,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: _page == 0 ? primaryColor : secondaryColor,
+            ),
+            label: "",
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+              color: _page == 1 ? primaryColor : secondaryColor,
+            ),
+            label: "",
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.add_circle,
+              color: _page == 2 ? primaryColor : secondaryColor,
+            ),
+            label: "",
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.favorite,
+              color: _page == 3 ? primaryColor : secondaryColor,
+            ),
+            label: "",
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+              color: _page == 4 ? primaryColor : secondaryColor,
+            ),
+            label: "",
+            backgroundColor: primaryColor,
+          ),
+        ],
+        onTap: navigationTapped,
       ),
     );
   }
